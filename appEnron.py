eca8d0ff2b3fd117592df05d555e2607f7262b13
jamespkelly20 <jamespkelly02@gmail.com>
2024-04-03 18:04:32 +0100

    Fixes

--- appEnron.py
+++ appEnron.py
@@ -8,7 +8,7 @@
 # import re
 
 # #sender_email = richard.shapiro@enron.com jeff.dasovich@enron.com
-# openai.api_key = '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'
+
 # # Streamlit UI
 # st.set_page_config(page_title="Email Summarization", page_icon=":tada:", layout="wide")
 
